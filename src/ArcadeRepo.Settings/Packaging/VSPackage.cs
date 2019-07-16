9e0ab78dcc999fa4dfe58015a5a3d92f5a0ace5e
Brad Marshall <bmarshall@sped.mobi>
2019-07-16 16:20:53 -0500

    updates

---
+++ src/ArcadeRepo.Settings/Packaging/VSPackage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using $(RootNamespace).Options;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace Microsoft.VisualStudio.Packaging
+{
+    [Guid("75E87631-D74D-4DC7-8EC2-8CEDC72F6489")]
+    [ProvideOptionPage(typeof(RepositoryGeneratorOptionPage), CategoryName, PageName, CategoryResourceID, PageNameResourceID, true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true, RegisterUsing = RegistrationMethod.CodeBase)]
+    public class $(SettingsPackageName) : AsyncPackage
+    {
+        public const string CategoryName = "$(OfficialName)";
+        public const string PageName = "$(OfficialName) Settings";
+        public const int CategoryResourceID = 0;
+        public const int PageNameResourceID = 0;
+
+    }
+}
